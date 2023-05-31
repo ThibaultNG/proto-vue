9d7e57f086fe4ef4cbb644db66b59d5ad8962c60
Thibault.NG <thibault.ng@etu.u-pec.fr>
2023-05-31 15:45:47 +0200

    Basic module architecture with meteo and album

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,14 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import Album from "../modules/album/views/Album.vue";
+import Meteo from "../modules/meteo/views/Meteo.vue";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
         {
-            path: "/",
-            name: "home",
-            component: HomeView
+            path: "/meteo",
+            name: "meteo",
+            component: Meteo
         },
         {
             path: "/about",
@@ -15,7 +16,7 @@ const router = createRouter({
             // route level code-splitting
             // this generates a separate chunk (About.[hash].js) for this route
             // which is lazy-loaded when the route is visited.
-            component: () => import("../views/AboutView.vue")
+            component: Album
         }
     ]
 });
