ab5ee329203c5c8c010e6edb1fcc5ebbdc2eb593
CamilleDU3 <camille3du@gmail.com>
2023-05-31 16:53:19 +0200

    first commit meteo

--- src/modules/meteo/views/Meteo.vue
+++ src/modules/meteo/views/Meteo.vue
@@ -1,11 +1,29 @@
 <script setup lang="ts">
+import {ref, reactive} from 'vue'
+import {getInfoMeteo} from "../service/meteoService"
+import axios from 'axios'
+
+const longitude = ref(1);
+const latitude = ref(-1);
+let test = ref(0)
+//const infoMeteo = getInfoMeteo();
+
+async function getInfoMeteoTmp(){
+    const resp = await axios.get("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current_weather=true&hourly=temperature_2m,relativehumidity_2m,windspeed_10m");
+    console.log(resp.data.current_weather.temperature);
+    test.value = resp.data.current_weather.temperature
+}
 
 </script>
 
 <template>
+    <label> longitude : </label>
+    <input v-model="longitude" type="number"/>
+    <label> latitude : </label>
+    <input v-model="latitude" type="number"/>
+    <br>
+    <p>{{ test }}</p>
+    <button @click="getInfoMeteoTmp">click</button>
 
+<p> {{ longitude }} + {{ latitude }}</p>
 </template>
-
-<style>
-	
-</style>
