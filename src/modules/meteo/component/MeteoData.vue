1efe451db44681b57f5dd752cb3d7dd797062064
CamilleDU3 <camille3du@gmail.com>
2023-06-01 11:23:10 +0200

    module meteo : generic display of attributs for the returned object

--- src/modules/meteo/component/MeteoData.vue
+++ src/modules/meteo/component/MeteoData.vue
@@ -6,19 +6,25 @@ const props = defineProps(['data', 'valuesPresent'])
 </script>
 
 <template>
-    
     <ul>
         <template v-if="valuesPresent">
-            <!-- <li v-for="item in props.data">
-                <p>valuesPresent = {{ valuesPresent }}</p>
                 <p>{{data.length}}</p>
-                <span v-for="(value, key, index) in item">
-                    {{key}}: {{value}} - {{index}}<br>    
+                <span v-for="(value, key, index) in props.data">
+                    <!-- <p>{{ typeof value }}</p> -->
+
+                    <template v-if="typeof value === 'object'">
+                        <span v-for="(valueBis, keyBis, indexBis) in value">
+                            [{{index}}.{{indexBis}}]{{keyBis}}: {{valueBis}}
+                        <br>
+                        </span>
+                    </template>
+                    <template v-else>
+                        [{{index}}]{{key}}: {{value}}
+                    </template>
+                    <br>
                 </span>
                 <br>
-            </li> -->
     </template>
     </ul>
     <!-- <button @click="seeDataPropContent"> click meteo data</button> -->
-    <div>props = {{ props }}</div>
 </template>
