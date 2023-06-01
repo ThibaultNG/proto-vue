d5f1e01fc04207a05ecdc7c7065a9bf59156073f
Thibault.NG <thibault.ng@etu.u-pec.fr>
2023-06-01 15:12:45 +0200

    search bar for game shops

--- src/modules/games/component/SelectGameShop.vue
+++ src/modules/games/component/SelectGameShop.vue
@@ -2,11 +2,15 @@
 import { computed, reactive, ref } from "vue";
 import { getShops } from "../service/gameService";
 
+const MAX_SELECTED = 3;
+
 const shops = ref(new Array());
-const selectedShops = ref([]);
+const selectedShop = ref(null);
+const searchShop = ref("");
+const loading = ref(false);
 
 const shopNames = computed(() =>
-    shops.value[0] ? shops.value[0].map((s: { [x: string]: any }) => s["storeName"]) : []
+	shops.value[0] ? shops.value[0].map((s: { [x: string]: any }) => s["storeName"]) : []
 );
 
 getShops().then((s) => shops.value.push(s));
@@ -14,18 +18,15 @@ getShops().then((s) => shops.value.push(s));
 </script>
 
 <template>
-    <v-container fluid>
-        <v-select v-model="selectedShops" :items="shopNames" label="Select stores" multiple>
-            <template v-slot:selection="{ item, index }">
-                <v-chip v-if="index < 4">
-                    <span>{{ item.title }}</span>
-                </v-chip>
-                <span v-if="index === 4" class="text-grey text-caption align-self-center">
-                    (+{{ selectedShops.length - 4 }} others)
-                </span>
-            </template>
-        </v-select>
-    </v-container>
-</template>
+	<v-toolbar>
+		<v-toolbar-title>Choose the shop</v-toolbar-title>
 
+		<v-autocomplete v-model="selectedShop" v-model:search="searchShop" :loading="loading" :items="shopNames" class="mx-4"
+			density="comfortable" hide-no-data hide-details label="Select shop"
+			style="max-width: 300px;"></v-autocomplete>
+
+		<v-btn icon="mdi-dots-vertical"></v-btn>
+	</v-toolbar>
+</template>
+  
 <style></style>
