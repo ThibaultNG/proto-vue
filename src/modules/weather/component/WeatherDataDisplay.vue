35a7dbb3379bc8b9b5f3b013c87a0be830b23ec4
CamilleDU3 <camille3du@gmail.com>
2023-06-06 10:15:33 +0200

    module meteo : adding types for props in WeatherDataDisplayValueIcon(old: WeatherDataValueWithIcon) part 2

--- src/modules/weather/component/WeatherDataDisplay.vue
+++ src/modules/weather/component/WeatherDataDisplay.vue
@@ -1,43 +1,43 @@
 <template>
 	<ul>
 		<template v-if="weatherStore.data != undefined && weatherStore.data != null">
-			<MeteoDataValueWithIcon
+			<WeatherDataDisplayValueIcon
 				v-if="weatherStore.data.current_weather.is_day"
 				icon="mdi-weather-sunny"
 				value="day time"
 				unit=""
-			></MeteoDataValueWithIcon>
-			<MeteoDataValueWithIcon
+			></WeatherDataDisplayValueIcon>
+			<WeatherDataDisplayValueIcon
 				v-else-if="!weatherStore.data.current_weather.is_day"
 				icon="mdi-weather-night"
 				value="night time"
 				unit=""
-			></MeteoDataValueWithIcon>
+			></WeatherDataDisplayValueIcon>
 			<br />
-			<MeteoDataValueWithIcon
+			<WeatherDataDisplayValueIcon
 				icon="mdi-clock-time-one-outline"
 				:value="weatherStore.data.current_weather.time"
 				unit=""
-			></MeteoDataValueWithIcon>
+			></WeatherDataDisplayValueIcon>
 			<br />
-			<MeteoDataValueWithIcon
+			<WeatherDataDisplayValueIcon
 				icon="mdi-sun-thermometer-outline"
 				:value="weatherStore.data.current_weather.temperature"
 				unit="°C"
-			></MeteoDataValueWithIcon>
+			></WeatherDataDisplayValueIcon>
 			<br />
-			<MeteoDataValueWithIcon
+			<WeatherDataDisplayValueIcon
 				icon="mdi-weather-windy"
 				:value="weatherStore.data.current_weather.windspeed"
 				unit="km/h"
-			></MeteoDataValueWithIcon>
+			></WeatherDataDisplayValueIcon>
 			<br />
 		</template>
 	</ul>
 </template>
 
 <script lang="ts" setup>
-import MeteoDataValueWithIcon from "./WeatherDataValueWithIcon.vue";
+import WeatherDataDisplayValueIcon from "./WeatherDataDisplayValueIcon.vue";
 import { useWeatherStore } from "../store/weatherStore";
 
 const weatherStore = useWeatherStore();
