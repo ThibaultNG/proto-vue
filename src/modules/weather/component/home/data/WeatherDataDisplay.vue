0a984be70d82545c80d2e0941d90d894d786632a
CamilleDU3 <camille3du@gmail.com>
2023-06-12 12:33:22 +0200

    module meteo: using default value in WeatherDataDisplayValueIcon's instance

--- src/modules/weather/component/home/data/WeatherDataDisplay.vue
+++ src/modules/weather/component/home/data/WeatherDataDisplay.vue
@@ -4,34 +4,34 @@
 		<WeatherDataDisplayValueIcon
 			v-if="weatherStore.data?.current_weather.is_day"
 			icon="mdi-weather-sunny"
-			:value="weatherStore.data ? 'day time' : ' --- '"
+			:value="weatherStore.data ? 'day time' : undefined"
 			unit=""
 		></WeatherDataDisplayValueIcon>
 		<WeatherDataDisplayValueIcon
 			v-else-if="!weatherStore.data?.current_weather.is_day"
 			icon="mdi-weather-night"
-			:value="weatherStore.data ? 'night time' : ' --- '"
+			:value="weatherStore.data ? 'night time' : undefined"
 			unit=""
 		></WeatherDataDisplayValueIcon>
 		<br />
 		<!-- TIME -->
 		<WeatherDataDisplayValueIcon
 			icon="mdi-clock-time-one-outline"
-			:value="weatherStore.data ? weatherStore.data.current_weather.time : ' --- '"
+			:value="weatherStore.data ? weatherStore.data.current_weather.time : undefined"
 			unit=""
 		></WeatherDataDisplayValueIcon>
 		<br />
 		<!-- TEMPERATURE -->
 		<WeatherDataDisplayValueIcon
 			icon="mdi-sun-thermometer-outline"
-			:value="weatherStore.data ? weatherStore.data.current_weather.temperature : ' --- '"
+			:value="weatherStore.data ? weatherStore.data.current_weather.temperature : undefined"
 			unit="°C"
 		></WeatherDataDisplayValueIcon>
 		<br />
 		<!-- WINDSPEED -->
 		<WeatherDataDisplayValueIcon
 			icon="mdi-weather-windy"
-			:value="weatherStore.data ? weatherStore.data.current_weather.windspeed : ' --- '"
+			:value="weatherStore.data ? weatherStore.data.current_weather.windspeed : undefined"
 			unit="km/h"
 		></WeatherDataDisplayValueIcon>
 		<br />
