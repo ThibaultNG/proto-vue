dedf5a23fa021f231cb33084b8aaf43465cdf0b6
CamilleDU3 <camille3du@gmail.com>
2023-06-14 10:23:42 +0200

    deleting unused files from previous commit

--- src/component/Footer.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <v-footer app>
-        <div class="text-center w-100">
-            {{ new Date().getFullYear() }} — <strong>DUNG</strong>
-        </div>
-    </v-footer>
-</template>
