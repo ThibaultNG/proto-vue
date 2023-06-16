0e38539996abb6cfd7cf2ebc3fc67155899da36b
CamilleDU3 <camille3du@gmail.com>
2023-06-16 08:31:23 +0200

    module meteo : changing from export const to Record for errorMessages and moving it to error package

--- src/modules/weather/error/serviceErrorHandler.ts
+++ src/modules/weather/error/serviceErrorHandler.ts
@@ -1,10 +1,23 @@
-import {
-	BAD_REQUEST,
-	INTERNAL_SERVER_ERROR,
-	NO_INTERNET_CONNECTION,
-	UNDEFINED_ERROR
-} from "../constants/errorMessages";
 import type ErrorInfo from "../models/ErrorInfo";
+
+const errorRecord: Record<string, ErrorInfo> = {
+	NO_INTERNET_CONNECTION: {
+		code: -1,
+		message: "Error : no internet connection"
+	},
+	UNDEFINED_ERROR: {
+		code: -100,
+		message: "Error : Please enter correct values"
+	},
+	BAD_REQUEST: {
+		code: 400,
+		message: "Error : Our data provider has an issue, please try again later.."
+	},
+	INTERNAL_SERVER_ERROR: {
+		code: 500,
+		message: "Error : Something happened, please try again later.."
+	}
+};
 
 export function setToNoError(errorInfo: ErrorInfo): ErrorInfo {
 	errorInfo.code = 0;
@@ -12,25 +25,24 @@ export function setToNoError(errorInfo: ErrorInfo): ErrorInfo {
 	errorInfo.happenedXTimes = 0;
 	return errorInfo;
 }
-
 export function handleErrorFromService(error: any, errorInfo: ErrorInfo): ErrorInfo {
-	let errorCode: number = UNDEFINED_ERROR.code;
-	let errorMessage: string = UNDEFINED_ERROR.message;
+	let errorCode: number = errorRecord.UNDEFINED_ERROR.code;
+	let errorMessage: string = errorRecord.UNDEFINED_ERROR.message;
 
 	if (error.response) {
 		// The request was made and the server responded with a status code(!= 2XX)
-		if (error.response.status == BAD_REQUEST.code) {
-			errorCode = BAD_REQUEST.code;
-			errorMessage = BAD_REQUEST.message;
-		} else if (error.response.status == INTERNAL_SERVER_ERROR.code) {
-			errorCode = INTERNAL_SERVER_ERROR.code;
-			errorMessage = INTERNAL_SERVER_ERROR.message;
+		if (error.response.status == errorRecord.BAD_REQUEST.code) {
+			errorCode = errorRecord.BAD_REQUEST.code;
+			errorMessage = errorRecord.BAD_REQUEST.message;
+		} else if (error.response.status == errorRecord.INTERNAL_SERVER_ERROR.code) {
+			errorCode = errorRecord.INTERNAL_SERVER_ERROR.code;
+			errorMessage = errorRecord.INTERNAL_SERVER_ERROR.message;
 		}
 	} else if (error.request) {
 		// The request was made but no response was received
 		if (String(error).includes("Network Error")) {
-			errorCode = NO_INTERNET_CONNECTION.code;
-			errorMessage = NO_INTERNET_CONNECTION.message;
+			errorCode = errorRecord.NO_INTERNET_CONNECTION.code;
+			errorMessage = errorRecord.NO_INTERNET_CONNECTION.message;
 		}
 	}
 
