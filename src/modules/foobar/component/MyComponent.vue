5b7fc18d26777c585b596d3e14d31f0859cd009b
ThibaultNG <byosoris@gmail.com>
2023-06-28 10:50:13 +0200

    component + model

---
+++ src/modules/foobar/component/MyComponent.vue
@@ -0,0 +1,19 @@
+<template>
+	<v-btn
+		prepend-icon="mdi-cursor-default-click-outline"
+		text="Click me"
+		color="primary"
+		@click="hello"
+	/>
+	<h1 v-if="showHello">HELLO !</h1>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const showHello = ref<boolean>(false);
+
+function hello() {
+	showHello.value = !showHello.value;
+}
+</script>
--- src/modules/foobar/views/FooPage.vue
+++ src/modules/foobar/views/FooPage.vue
@@ -1,3 +1,8 @@
 <template>
 	<h1>Foooo !</h1>
+	<MyComponent />
 </template>
+
+<script setup lang="ts">
+import MyComponent from "../component/MyComponent.vue";
+</script>
